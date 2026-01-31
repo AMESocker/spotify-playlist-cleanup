c9d89e0f59fa3412c660beaf598d820268fb5020
Aaron Socher <aesocher@yahoo.com>
2026-01-30 19:00:53 -0800

    new two run format

--- scheduler.js
+++ scheduler.js
@@ -3,8 +3,14 @@ import cron from 'node-cron';
 import { logInfo } from './logger.js';
 
 export function scheduleDaily(jobFn) {
-  cron.schedule('0 2,15 * * *', () => {
-    logInfo('Daily cleanup at ' + new Date().toISOString());
+  cron.schedule('0 3 * * *', () => {
+    logInfo('Daily cleanup (3 AM) at ' + new Date().toISOString());
+    jobFn();
+  });
+  
+  cron.schedule('0 13 * * *', () => {
+    logInfo('Daily cleanup (1 PM) at ' + new Date().toISOString());
     jobFn();
   });
 }
+
