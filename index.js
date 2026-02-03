e2c02d64034355786310cfa2f4d927de5971d645
Aaron Socher <aesocher@yahoo.com>
2026-02-02 18:52:44 -0800

    Removed extra addNextAlbum

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { initAuthIfNeeded } from './auth.js';
 import { runJob } from './job.js';
 import { scheduleDaily } from './scheduler.js';
 import { logInfo } from './logger.js';
-import { addNextAlbum } from './curator.js';
 
 (async () => {
   const ready = await initAuthIfNeeded();
@@ -12,7 +11,6 @@ import { addNextAlbum } from './curator.js';
 
   if (process.env.RUN_MODE === 'once') {
     await runJob();
-    await addNextAlbum(); // add album as part of the daily job
   } else {
     scheduleDaily(runJob);
     logInfo('Scheduled daily cleanup job.');
